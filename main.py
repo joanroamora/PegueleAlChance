7f33840f688962e142aebe642fdbb07cab8a2d59
JOAN ROA <joanroamora@gmail.com>
2022-05-18 21:46:01 -0500

    C1: interfaz al main.

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 ##Tkinter es una libreria que nos permite gestionar Interfaces graficas en Python
-from ctypes import alignment
-from msilib.schema import Icon
-from operator import iconcat
 from tkinter import *
 import random
-##Interfaz gráfica en Tkinter
-raiz = Tk()
-raiz.resizable(0,0)
-raiz.config(bg="red")
-raiz.geometry('320x540')
-raiz.iconbitmap(default="layers/titleicon.ico")
 
-unFrame = Frame()
-unFrame.pack()
-unFrame.config(width=320, height=540)
-unFrame.config(bd=0)
-
-fondo = PhotoImage(file="layers/android.png")
-miLabel = Label(unFrame, image=fondo, cursor="shuttle").place(x=0,y=0)
-
-miLabel2=Label(unFrame, text="El Genio Virtual", bg = "black", fg="white", font=('arial', 18)).place(x=80,y=30)
-
-miLabel3=Label(unFrame, text="Genio quiere ayudarte a ser millonario,\n indicale tú número ganador, él te correjira. \n Solo 4 dígitos... :", bg = "black", fg="white", 
-font=('arial', 10) , anchor="center").place(x=38,y=75)
-
-
-num = IntVar ()
-cuadroEntrada = Entry(unFrame, width=6, cursor="trek", font=('arial', 20)).place(x=120,y=165)
-cuadroEntrada = Entry(textvariable=num)
-
-botonEnviar = Button(unFrame, text="PÉGUELE AL CHANCE").place(x=105,y=215)
-
-raiz.mainloop()
 
 if __name__ == "__main__":
-    
+    ##Interfaz gráfica en Tkinter
+    raiz = Tk()
+    raiz.resizable(0,0)
+    raiz.config(bg="red")
+    raiz.geometry('320x540')
+    raiz.iconbitmap(default="layers/titleicon.ico")
+
+    unFrame = Frame()
+    unFrame.pack()
+    unFrame.config(width=320, height=540)
+    unFrame.config(bd=0)
+
+    fondo = PhotoImage(file="layers/android.png")
+    miLabel = Label(unFrame, image=fondo, cursor="shuttle").place(x=0,y=0)
+
+    miLabel2=Label(unFrame, text="El Genio Virtual", bg = "black", fg="white", font=('arial', 18)).place(x=80,y=30)
+
+    miLabel3=Label(unFrame, text="Genio quiere ayudarte a ser millonario,\n indicale tú número ganador, él te correjira. \n Solo 4 dígitos... :", bg = "black", fg="white", 
+    font=('arial', 10) , anchor="center").place(x=38,y=75)
+
+
+    num = IntVar ()
+    cuadroEntrada = Entry(unFrame, width=6, cursor="trek", font=('arial', 20)).place(x=120,y=165)
+    cuadroEntrada = Entry(textvariable=num)
+
+    botonEnviar = Button(unFrame, text="PÉGUELE AL CHANCE").place(x=105,y=215)
+
+    raiz.mainloop()
+
     primerDig = random.randint(0,9)
     segunDig = random.randint(0,9)
     tercerDig = random.randint(0,9)
