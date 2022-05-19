5185b0c1a8b4e36fa17a7fbc21e94b8d2b252203
JOAN ROA <joanroamora@gmail.com>
2022-05-19 00:39:20 -0500

    C4:version final, funcionando. Se comparte a master

--- main.py
+++ main.py
@@ -24,14 +24,9 @@ if __name__ == "__main__":
     miLabel3=Label(unFrame, text="Genio quiere ayudarte a ser millonario,\n indicale tú número ganador, él te correjira. \n Solo 4 dígitos... :", bg = "black", fg="white", 
     font=('arial', 10) , anchor="center").place(x=38,y=75)
 
+    num = StringVar ()
+    cuadroEntrada = Entry(unFrame, width=6, cursor="trek", textvariable=num, font=('arial', 20)).place(x=120,y=165)
 
-    num = IntVar ()
-    cuadroEntrada = Entry(unFrame, width=6, cursor="trek", font=('arial', 20)).place(x=120,y=165)
-    cuadroEntrada = Entry(textvariable=num)
-
-    botonEnviar = Button(unFrame, text="PÉGUELE AL CHANCE").place(x=105,y=215)
-
-    raiz.mainloop()
 
     primerDig = random.randint(0,9)
     segunDig = random.randint(0,9)
@@ -39,27 +34,37 @@ if __name__ == "__main__":
     cuartoDig = random.randint(0,9)
 
     sumaDigMachine = (primerDig * 1000)+(segunDig * 100)+(tercerDig*10)+(cuartoDig*1)
-    intento = 0
-    while True:
-        entradaDig = input ('Por favor ingrese su número ganador :')
-        ##List comprehensions
-        x = [int(a) for a in str(entradaDig)]
+
+    
+    
+    def candela():
+        numero = num.get()
+        print(numero)
+        x = [int(a) for a in str(numero)]
         print(x)
+
         sumaDigUser = (x[0] * 1000) + (x[1] * 100) + (x[2] * 10) + (x[3] * 1)
-        1231
+
         if sumaDigUser > sumaDigMachine:
             print ("¡Réstele!")
-            intento = intento + 1
-            print (intento)
+            miLabel3=Label(unFrame, text="¡Réstele!", bg = "black", fg="white", font=('arial', 18)).place(x=120,y=300)
         elif sumaDigUser < sumaDigMachine:
             print ("¡Súmele!")
-            intento = intento + 1
-            print (intento)
+            miLabel3=Label(unFrame, text="¡Súmele!", bg = "black", fg="white", font=('arial', 18)).place(x=120,y=300)
         else:
-            if ( x[0] == 0 ): {
-            print ("El numero ganador es: " + f'0{sumaDigMachine}' + " vaya y lo hace!!!")
-            }
-            else: {
-            print ("El numero ganador es: " + f'{sumaDigMachine}' + " vaya y lo hace!!!")
-            }
-            break
+            if ( x[0] == 0 ): 
+                print ("El numero ganador es: " + f'0{sumaDigMachine}' + " vaya y lo hace!!!")
+                miLabel3=Label(unFrame, text="El numero ganador es: " + f'0{sumaDigMachine}' + "\n vaya y lo hace!!!", bg = "black", fg="red", font=('arial', 20)).place(x=120,y=300)       
+            else: 
+                print ("El numero ganador es: " + f'{sumaDigMachine}' + " vaya y lo hace!!!")
+                miLabel3=Label(unFrame, text="El numero ganador es: " + f'{sumaDigMachine}' + " \n vaya y lo hace!!!", bg = "black", fg="red", font=('arial', 20)).place(x=120,y=300)
+
+
+    botonEnviar = Button(unFrame, text="PÉGUELE AL CHANCE", command= candela).place(x=105,y=215)
+    
+    fondo2 = PhotoImage(file="layers/python.png")
+    miLabel4=Label(unFrame, image=fondo2).place(x=95,y=350)
+
+   
+
+    raiz.mainloop()
